9b8f157aa04d70b089a5e86b6c6ba0966753b6b4
Raphaela <raphaela.dohrmann@gmail.com>
2022-12-16 16:09:50 +0100

    Implement delete

--- frontend/src/components/ToDoCard.tsx
+++ frontend/src/components/ToDoCard.tsx
@@ -1,16 +1,24 @@
 import {Link} from "react-router-dom";
+import axios from "axios";
 
-export default function ToDoCard({ description, id, status} : {
+export default function ToDoCard({description, id, status}: {
     description: string,
     id: string,
     status: string
 }) {
+    async function deleteItem() {
+        const response = await axios.delete("/api/todo/" + id);
+        console.log(response.status)
+        response.status === 200 && window.location.reload();
+    }
+
     return (
         <div className={"todo-card"}>
             <h2>{description}</h2>
             <div className={"todo-card--actions"}>
                 <p>Status: {status}</p>
                 <p><Link to={"/todo/" + id}>Bearbeiten</Link></p>
+                <button onClick={() => deleteItem()}>Löschen</button>
             </div>
         </div>
     )
